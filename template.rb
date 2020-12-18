99b8141d13c66dc6bd2890a2f284e3878658848c
khoa <mkhoa1412@gmail.com>
2020-12-18 16:35:09 +0700

    update

--- template.rb
+++ template.rb
@@ -36,20 +36,20 @@ def apply_template!
 
   template "lib/tasks/redis.rake.tt"
 
+  insert_into_file "config/application.rb",  
+    after: /^  module\s*$/ do
+      [
+        "Dotenv::Railtie.load",
+        "HOSTNAME = ENV['HOSTNAME']"
+      ].join("\n") + "\n"
+    end
+
   insert_into_file "config/application.rb",
     before: /^  end\s*$/ do
       [
         "    # We will use the fully-armed and operational power of Postgres",
         "    # and that means using SQL-based structure.",
         "    config.active_record.schema_format = :sql"
-      ].join("\n") + "\n"
-    end
-    
-  insert_into_file "config/application.rb",  
-    after: /^  Bundler.require\s*$/ do
-      [
-        "Dotenv::Railtie.load",
-        "HOSTNAME = ENV['HOSTNAME']"
       ].join("\n") + "\n"
     end
 

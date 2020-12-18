329af3ec2632d51174f077eb12209ee69fa4ec06
khoa <mkhoa1412@gmail.com>
2020-12-18 17:34:45 +0700

    update

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,16 @@
+
+REDIS_DATABASE_SIDEKIQ = ENV['REDIS_DATABASE_SIDEKIQ'] || 1
+REDIS_HOST = ENV['REDIS_HOST'] || 'localhost'
+REDIS_PORT = ENV['REDIS_PORT'] || '6379'
+
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: ENV.fetch("SIDEKIQ_REDIS_URL")
+    url: "redis://#{REDIS_HOST}:#{REDIS_PORT}/#{REDIS_DATABASE_SIDEKIQ}"
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: ENV.fetch("SIDEKIQ_REDIS_URL")
+    url: "redis://#{REDIS_HOST}:#{REDIS_PORT}/#{REDIS_DATABASE_SIDEKIQ}"
   }
 end
